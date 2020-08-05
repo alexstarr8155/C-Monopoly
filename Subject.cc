6d579b02dbe5431fe346dc1bd9389a8845f1a9c7
a2starr <a2starr@student.cs.uwaterloo.ca>
2020-08-05 16:29:01 -0400

    Implementation of the Subject superclass

---
+++ Subject.cc
@@ -0,0 +1,12 @@
+#include "Subject.h"
+
+void Subject::attach(std::shared_ptr<Observer> o) {
+	observers.push_back(o);
+}
+
+void Subject::notifyObservers() {
+	
+	for (int i = 0; i < observers.size(); i++) {
+		observers[i]->notify(*this);
+	}
+}
