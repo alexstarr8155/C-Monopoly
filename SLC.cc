7007e4d0675359435b9a6909c893175c66e3b439
a2starr <a2starr@student.cs.uwaterloo.ca>
2020-08-09 01:56:59 -0400

    Implemented SLC.cc

--- SLC.cc
+++ SLC.cc
@@ -1,5 +1,26 @@
 #include "SLC.h"
+#include <stdlib.h>
 
 void SLC::action(std::shared_ptr<Player> p, bool b) {
+	int chance = rand() % 24;
 
+	if (chance < 3) {
+		p->move(-3);
+	} else if (chance >= 3 && chance < 7) {
+		p->move(-2);
+	} else if (chance >= 7 && chance < 11) {
+		p->move(-1);
+	} else if (chance >= 11 && chance < 14) {
+		p->move(1);
+	} else if (chance >= 14 && chance < 18) {
+		p->move(2);
+	} else if (chance >= 18 && chance < 22) {
+		p->move(3);
+	} else if (chance == 22) {
+		p->moveTo(10);
+	} else if (chance == 23) {
+		p->moveTo(0);
+	} else {
+		throw "Ya done goofed";
+	}
 }
