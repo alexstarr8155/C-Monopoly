2dd86179f5c0454c4c744ac67e004e38272aa605
a2starr <a2starr@student.cs.uwaterloo.ca>
2020-08-09 03:04:40 -0400

    Made the Needles Hall subclass

---
+++ NeedlesHall.cc
@@ -0,0 +1,26 @@
+#include "NeedlesHall.h"
+#include <stdlib.h>
+
+void NeedlesHall::action(std::shared_ptr<Player> p, bool b) {
+	
+	int chance = rand() % 18;
+
+	if (chance < 1) {
+		p->removeMoney(200);
+	} else if (chance < 3) {
+		p->removeMoney(100);
+	} else if (chance < 6) {
+		p->removeMoney(50);
+	} else if (chance < 12) {
+		p->addMoney(25);
+	} else if (chance < 15) {
+		p->addMoney(50);
+	} else if (chance < 17) {
+		p->addMoney(100);
+	} else if (chance < 18) {
+		p->addMoney(200);
+	} else {
+		throw "Random Number failed";
+	}
+
+}
---
+++ NeedlesHall.h
@@ -0,0 +1,8 @@
+#include "Cell.h"
+#include "Player.h"
+
+class NeedlesHall : public Cell {
+	public:
+		virtual void action(std::shared_ptr<Player> p, bool b);
+		virtual void notify(Subject & whoNotified) override { /* "Don't Need" */ };
+};
