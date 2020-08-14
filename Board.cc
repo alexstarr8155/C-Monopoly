06299de5f099d7f4e8199cacc03ab176ebd38e26
jkodeswa <jkodeswa@student.cs.uwaterloo.ca>
2020-08-14 05:29:13 -0400

    modified:   Board.cc

--- Board.cc
+++ Board.cc
@@ -71,6 +71,10 @@ Board::Board(std::string fileName) {
         players[i]->setTurnsInTims(turnsInTims);
 
         board[position]->setOnCell(players[i]->getPlayerChar());
+
+	for (int i = 0; i < playerNum; ++i) {
+		board[0]->leave(players[i]);
+	}
     }
 
     for (int i = 0; i < 40; i++) {
