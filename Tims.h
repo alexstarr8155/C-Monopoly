50a6e1c7186a50d228b361bcc797dcdf3c99c3b6
Jathushan Kodeswaran <jay@Jathushans-MacBook-Pro.local>
2020-08-15 06:00:13 -0400

    modified:   Tims.cc 	modified:   Tims.h 	modified:   Tuition.cc 	modified:   Tuition.h

--- Tims.h
+++ Tims.h
@@ -1,13 +1,13 @@
 #ifndef _TIMS_H_
 #define _TIMS_H_
-
 #include "Cell.h"
+
+/* this class represents the DC Tims Line Tile of the Board */
 
 class Tims: public Cell {
 	public:
 		Tims();
-		virtual void action (std::shared_ptr<Player> p, bool b) override;
-
+		virtual void action(std::shared_ptr<Player> p, bool b) override;
 };
 
 #endif
