b3bf2259d32451697d14222b7f0829232bc29d25
Jathushan Kodeswaran <jay@Jathushans-MacBook-Pro.local>
2020-08-15 14:57:34 -0400

    modified:   Player.cc 	modified:   Player.h

--- Player.cc
+++ Player.cc
@@ -1,43 +1,20 @@
-#include "Player.h"
 #include <algorithm>
 #include <iostream>
+#include "Player.h"
 #include "Improvable.h"
 #include "BankruptException.h"
 
-Player::Player (const std::string &name, char playerChar, int money): name{name}, playerChar{playerChar}, money{money}, netWorth{money} {
+// the initial construction of the Player class
+Player::Player (const std::string &name, char playerChar, int money) : name{name}, playerChar{playerChar}, money{money}, netWorth{money} {
 	position = 0;
 }
 
-char Player::getPlayerChar() const {
-	return playerChar;
-}
-
-std::string Player::getPlayerName() const {
-	return name;
-}
-
-int Player::getMoney() const {
-	return money;
-}
-
-//"Perhaps this should return a shared pointer instead?"
-std::vector<Property *> Player::getProperties() const{
-	return owned;
-}
-
-void Player::updateNetWorth() {
-	int worth = 0;
-
-	for (int i = 0; i < owned.size(); i++) {
-		worth += owned[i]->getValue();
-	}
-	netWorth = worth + money;
-}
-
+// player is in DC Tims Line (aka jail)
 void Player::goToTims() {
-	inTims = true;
-	//moveTo(10);	
+	inTims = true;	
 }
+
+// player is out of DC Tims Line (aka jail)
 void Player::getOutOfTims(bool bail) {
 
 	if (!inTims) {
@@ -52,26 +29,18 @@ void Player::getOutOfTims(bool bail) {
 	inTims = false;
 }
 
-int Player::getTimsCards() {
-	return hasTimsCard;
-}
-
+// increases the number of Tims Cards that the player has by 1
 void Player::addTimsCard (){
 	hasTimsCard++;
 }
 
-bool Player::getInTims() {
-	return inTims;
-}
-
-int Player::getTurnsInTims() {
-	return turnsInTims;
-}
-
+// increases the player's available money by the value of 'amount'
 void Player::addMoney(int amount) {
 	money += amount;
-} 
+}
 
+// decreases the player's available money by the value of 'amount'
+// if player does not have enough money to withdraw will thrown an exception
 void Player::removeMoney(int amount) {
 	if (amount > money) {
 		throw BankruptException(amount - money, nullptr);
@@ -79,15 +48,18 @@ void Player::removeMoney(int amount) {
 	money -= amount;
 }
 
-int Player::getNetWorth () {
-	updateNetWorth();
-	return netWorth;
+// updates the Net Worth of Player
+void Player::updateNetWorth() {
+	int worth = 0;
+
+	for (int i = 0; i < owned.size(); i++) {
+		worth += owned[i]->getValue();
+	}
+	netWorth = worth + money;
 }
 
-int Player::getPosition() const {
-	return position;
-}
-
+// updates the player's position on the board by the value of 'amount'
+// if player pass the Collect Osap Tile in board increases the player's available money by $200
 void Player::move(int amount) {
 	if (position + amount < 0) {
 		position = position + amount + 40;
@@ -100,10 +72,12 @@ void Player::move(int amount) {
 	position = (position + amount) % 40;
 }
 
+// will move the player to a specific part of the board (dependent on 'pos')
 void Player::moveTo(int pos) {
 	position = pos;
 }
 
+// returns a bool representing if all the properties owned by player are mortgaged
 bool Player::allPropertiesAreMortgaged() {
 	
 	bool mortgaged = true;
@@ -112,9 +86,11 @@ bool Player::allPropertiesAreMortgaged() {
 	}
 
 	return mortgaged;
-
 }
 
+// will check if the player is able to buy the property
+// if not will throw an exception
+// if they are will update the vector representing the properties that the player owns
 void Player::buy(Property * p) {
 	if (p->getPrice() > money){
 		throw "Not enough money";
@@ -123,6 +99,8 @@ void Player::buy(Property * p) {
 	owned.push_back(p);
 }
 
+// player will attempt to pay 'other' the value of 'amount'
+// if they cannot will throw an exception
 void Player::pay(std::shared_ptr<Player> other, int amount) {
 	if (amount > money) {
 		throw BankruptException(amount - money, other);
@@ -171,8 +149,6 @@ void Player::trade(std::shared_ptr<Player> other, Property* mine, Property* othe
 	auto first = find(owned.begin(), owned.end(), mine);
 	auto second = find(other->owned.begin(), other->owned.end(), others);
 
-	//std::cout << "    " << mine->getName() << ", " << others->getName() << std::endl;
-
 	if (first == owned.end()) {
 		//"Throw some exception, since this player does not have the property to trade"
 //		std::cout << "First = ownded.end()" << std::endl;
@@ -206,40 +182,90 @@ void Player::trade(std::shared_ptr<Player> other, Property* mine, Property* othe
 		//other->owned.erase(second);
 
 	}
-
 }
 
+// set the name member of player
 void Player::setName(std::string name) {
 	this->name = name;
 }
 
+// set the playerChar member of player
 void Player::setPlayerChar(char playerChar) {
 	this->playerChar = playerChar;
 }
 
+// set the money member of player
 void Player::setMoney(int money) {
 	this->money = money;
 }
 
+// set the position member of player
 void Player::setPosition(int position) {
 	this->position = position;
 }
 
+// set the hasTimsCard member of player
 void Player::setTimsCups(int timsCups) {
 	this->hasTimsCard = timsCups;
 }
 
+// set the inTims member of player
 void Player::setInTims(bool inTims) {
 	this->inTims = inTims;
 }
 
+// set the turnsInTims member of player
 void Player::setTurnsInTims(int turnsInTims) {
 	this->turnsInTims = turnsInTims;
 }
 
+// adds a property to the owned memeber of player
 void Player::addProperty(Property *property) {
 	(this->owned).push_back(property);
 }
 
+// returns the player's name
+std::string Player::getPlayerName() const {
+	return name;
+}
 
+// returns the player's character
+char Player::getPlayerChar() const {
+	return playerChar;
+}
 
+// returns the player's avaliable money
+int Player::getMoney() const {
+	return money;
+}
+
+// returns the player's current position
+int Player::getPosition() const {
+	return position;
+}
+
+// updates the player's net worth and then returns it
+int Player::getNetWorth () {
+	updateNetWorth();
+	return netWorth;
+}
+
+// returns the number of Tims Cards that the player has
+int Player::getTimsCards() {
+	return hasTimsCard;
+}
+
+// returns if player is in DC Tims Line (aka jail) or not
+bool Player::getInTims() {
+	return inTims;
+}
+
+// returns the number of turns that player has been in jail
+int Player::getTurnsInTims() {
+	return turnsInTims;
+}
+
+// returns a vector of properties that the player owns
+std::vector<Property *> Player::getProperties() const{
+	return owned;
+}
