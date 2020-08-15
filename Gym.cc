3deced71de79ad38b4477dfb0f6f7dc8c075ea59
JamesHoulee <jmhoule10@hotmail.com>
2020-08-15 05:47:52 -0400

    Changes to be committed: 	modified:   Gym.cc

--- Gym.cc
+++ Gym.cc
@@ -1,4 +1,8 @@
 #include "Gym.h"
+#include "Player.h"
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
 Gym::Gym (const string &name, vector<string> set) : Property{name, 150, set} {
@@ -7,5 +11,28 @@ Gym::Gym (const string &name, vector<string> set) : Property{name, 150, set} {
 
 // returns the cost of landing on the gym
 int Gym::getRent() const {
-	return -666; //temp, add calc
+	
+	srand ((unsigned int)time(NULL));
+
+	int dice1 = 1 + (rand() % 6);
+	int dice2 = 1 + (rand() % 6);
+	cout << dice1 << " " << dice2 << endl;
+
+	int sum = dice1 + dice2;
+
+	int numOwned = 0;
+	for (auto it = set_ownership.begin(); it != set_ownership.end(); ++it) {
+		if (it->second == owner->getPlayerChar()){
+			numOwned++;
+		}
+	}
+
+	switch (numOwned) {
+		case 1:
+			return sum * 4;
+		case 2:
+			return sum * 10;
+	}
+	
+
 }
