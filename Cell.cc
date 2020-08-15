06d3a864c034bd9aff929aae20595e94ed31c04c
Jathushan Kodeswaran <jay@Jathushans-MacBook-Pro.local>
2020-08-15 04:21:28 -0400

    modified:   Cell.cc 	modified:   Cell.h

--- Cell.cc
+++ Cell.cc
@@ -2,6 +2,7 @@
 #include "Player.h"
 using namespace std;
 
+/* initial construction of the Cell class where there is no one on the cell */
 Cell::Cell (const string & name): name{name} {
 	numImprovements = -2;
 	on_cell = {
@@ -16,44 +17,54 @@ Cell::Cell (const string & name): name{name} {
 	};
 }
 
-string Cell::getName () const {
+// returns the name of the cell
+string Cell::getName() const {
 	return name;
 }
 
-void Cell::leave (shared_ptr<Player> p){ 
-	on_cell.at(p->getPlayerChar ()) = false;
-}
-
-map<char, bool> Cell::who_on_cell () {
-	return on_cell;
-}
-
-int Cell::getNumImprov () const {
-	return numImprovements;
-}
-
-void Cell::setNumImprov (int num) {
-	numImprovements = num;
-}
-
-
-void Cell::setOwner (std::shared_ptr<Player> p) {
-	owner = p;
-}
-
-shared_ptr<Player> Cell::getOwner () {
-	return owner;
-}
-
+// the a player with 'playerChar' as character is on the cell
 void Cell::setOnCell(char playerChar) {
 	on_cell[playerChar] = true;
 }
 
-/* moved from property */
+// returns the map representing which players are on the cell
+map<char, bool> Cell::who_on_cell() {
+	return on_cell;
+}
+
+// the player 'p' is now not on the cell
+void Cell::leave(shared_ptr<Player> p){ 
+	on_cell.at(p->getPlayerChar ()) = false;
+}
+
+// sets the number of improvements that Cell should have
+void Cell::setNumImprov (int num) {
+	this->numImprovements = num;
+}
+
+// returns the number of improvements on such cell
+// Note: really only applicable for improvableProperty
+int Cell::getNumImprov () const {
+	return numImprovements;
+}
+
+/// set player 'p' to be the owner of Cell
+void Cell::setOwner (std::shared_ptr<Player> p) {
+	this->owner = p;
+}
+
+// returns the owner of Cell
+shared_ptr<Player> Cell::getOwner () {
+	return owner;
+}
+
+// sets if the Cell is mortgaged or not
+void Cell::setMortgage(bool isMortgaged) {
+	this->mortgaged = isMortgaged;
+}
+
+// returns if Cell is mortgaged or not
 bool Cell::is_mortgaged () const {
 	return mortgaged;
 }
 
-void Cell::setMortgage(bool isMortgaged) {
-	this->mortgaged = isMortgaged;
-}
--- Cell.h
+++ Cell.h
@@ -1,39 +1,43 @@
 #ifndef _CELL_H_
 #define _CELL_H_
-
 #include <memory>
 #include <string>
 #include <map>
 #include <iostream>
 
-
 class Player;
 
+/* this class represents each tile in the games (properites, collect osap etc) */
+
 class Cell {
+
 	protected:
-		std::string name;
-		std::map<char, bool> on_cell;
-		int numImprovements;
-		std::shared_ptr<Player> owner;
-		bool mortgaged; // moved from Property
-	public:
-		Cell (const std::string &name);
-
-		std::string getName () const;
+		std::string name; // name of cell
+		int numImprovements; // number of improvements on a cell
+		bool mortgaged; // bool representing if a cell is mortgaged or not
 		
-		virtual void action (std::shared_ptr<Player> p, bool b) = 0;
-		void leave (std::shared_ptr<Player> p);
+		std::map<char, bool> on_cell; // a map representing who is on the cell and who is not on the cell
+		std::shared_ptr<Player> owner; // represents which player owns the cell
 
-		std::map<char, bool> who_on_cell ();
-		int getNumImprov () const;
-		void setNumImprov (int num);
+	public:
+		
+		Cell(const std::string &name);
+		virtual void action(std::shared_ptr<Player> p, bool b) = 0;
 
-		virtual void setOwner (std::shared_ptr<Player> p);
-		std::shared_ptr<Player> getOwner ();
-
+		std::string getName() const;
+		
 		void setOnCell(char playerChar);
-		bool is_mortgaged () const; // moved from Property
+		std::map<char, bool> who_on_cell();
+		void leave(std::shared_ptr<Player> p);
+
+		void setNumImprov(int num);
+		int getNumImprov() const;
+
+		virtual void setOwner(std::shared_ptr<Player> p);
+		std::shared_ptr<Player> getOwner();
+		
 		void setMortgage(bool isMortgaged);
+		bool is_mortgaged() const;
 };
 
 #endif
